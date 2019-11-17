6ad25749c09f87960c2de15d30d5d7c6fc18c70e
jjerion <56169080+jjerion@users.noreply.github.com>
2019-11-16 22:44:24 -0500

    added timer script

---
+++ Timer Assignment Jess+Sasha/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public GameObject timerObject; //var for timer itself
+    Text timerTextComp; //variable for text component
+    int timer = 0; //starting timer value
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerTextComp = timerObject.GetComponent<Text>(); 
+        timerTextComp.text = "0"; 
+        InvokeRepeating("UpdateText", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateText()
+    {
+        timer++; //increase the timer
+        timerTextComp.text = "Time: " + timer; 
+    }
+
+}
